c9c5699b3b471316aa61f65207f7118afe9a76fa
Julian Kuners <julian.kuners@gmail.com>
2025-05-30 12:47:36 +0200

    add `komet.rust-soroban` derivation

--- flake.nix
+++ flake.nix
@@ -8,20 +8,45 @@
     flake-utils.follows = "k-framework/flake-utils";
     rv-utils.follows = "k-framework/rv-utils";
     poetry2nix.follows = "k-framework/poetry2nix";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
+
+    stellar-cli-flake.url = "github:stellar/stellar-cli";
+    stellar-cli-flake.inputs = {
+      flake-utils.follows = "flake-utils";
+      nixpkgs.follows = "nixpkgs";
+      rust-overlay.follows = "rust-overlay";
+    };
   };
 
   outputs = { self, k-framework, nixpkgs, flake-utils, rv-utils, wasm-semantics
-    , rust-overlay, ... }@inputs: flake-utils.lib.eachSystem [
+    , rust-overlay, stellar-cli-flake, ... }@inputs: flake-utils.lib.eachSystem [
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-linux"
       "aarch64-darwin"
     ] (system:
       let
+        # stellar-cli flake does not build on NixOS machines due to openssl issues during `cargo build`
+        # putting `pkg-config` in `nativeBuildInputs` will run the `pkg-config` setuphook, which will look for derivations in `buildInputs`
+        # with a `pkgconfig` directory such as the `openssl` derivation
+        # this will then setup the `PKG_CONFIG_PATH` env variable properly
+        stellar-cli-overlay = final: prev: {
+          stellar-cli = stellar-cli-flake.packages.${system}.default.overrideAttrs (finalAttrs: previousAttrs: {
+            nativeBuildInputs = (previousAttrs.nativeBuildInputs or [ ]) ++ (with final; [
+              pkg-config
+            ]);
+            buildInputs = (previousAttrs.buildInputs or [ ]) ++ (with final; [
+              openssl
+            ]);
+          });
+        };
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [
+            stellar-cli-overlay
+            (import rust-overlay)
+          ];
         };
 
         poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
@@ -44,7 +69,7 @@
           checkGroups = [ ];
         };
 
-        komet = pkgs.stdenv.mkDerivation {
+        mkKomet = {komet-rust ? null, komet-soroban ? null}@args: pkgs.stdenv.mkDerivation {
           pname = "komet";
           version = self.rev or "dirty";
           src = pkgs.lib.cleanSource (pkgs.nix-gitignore.gitignoreSourcePure [
@@ -80,56 +105,30 @@
             cp -r ./kdist-*/* $out/
 
             makeWrapper ${komet-pyk}/bin/komet $out/bin/komet --prefix PATH : ${
-              pkgs.lib.makeBinPath [ k-framework.packages.${system}.k ]
+              pkgs.lib.makeBinPath (
+                [
+                  k-framework.packages.${system}.k
+                ] ++ pkgs.lib.optionals (komet-rust != null) [
+                  komet-rust
+                ] ++ pkgs.lib.optionals (komet-soroban != null) [
+                  komet-soroban
+                ]
+              )
             } --set KDIST_DIR $out
           '';
+
+          passthru = if komet-rust == null && komet-soroban == null then {
+            rust-soroban = pkgs.callPackage mkKomet (args // {
+              komet-rust = rustWithWasmTarget;
+              komet-soroban = pkgs.stellar-cli;
+            });
+          } else { };
         };
+        komet = pkgs.callPackage mkKomet { };
 
         rustWithWasmTarget = pkgs.rust-bin.stable.latest.default.override {
           targets = [ "wasm32v1-none" ];
         };
-
-        rustPlatformWasm = pkgs.makeRustPlatform {
-          cargo = rustWithWasmTarget;
-          rustc = rustWithWasmTarget;
-        };
-
-        version = "21.4.0";
-        stellar-src = pkgs.fetchFromGitHub {
-          owner = "stellar";
-          repo = "stellar-cli";
-          rev = "v${version}";
-          hash = "sha256-yPg0Tsnb7H7S1MbVvfWrAmTWehWqwJYSqYLqLWVNq0Y=";
-        };
-
-        stellar-cli = rustPlatformWasm.buildRustPackage rec {
-          pname = "stellar-cli";
-          inherit version;
-          src = stellar-src;
-
-          nativeBuildInputs = [ pkgs.pkg-config ]
-            ++ (if pkgs.stdenv.isDarwin then
-              [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ]
-            else
-              [ ]);
-
-          buildInputs = [ pkgs.openssl pkgs.openssl.dev ];
-
-          OPENSSL_NO_VENDOR = 1;
-          GIT_REVISION = "v${version}";
-
-          doCheck = false;
-
-          cargoLock = {
-            lockFile = "${stellar-src}/Cargo.lock";
-            outputHashes = {
-              "testcontainers-0.15.0" =
-                "sha256-v9HJ0cgDgTCRwB6lPm425EmVq3L9oNI8NVCzv4T2HOQ=";
-            };
-          };
-
-        };
-
       in {
         packages = rec {
           inherit komet komet-pyk;
@@ -137,7 +136,7 @@
         };
 
         devShell = pkgs.mkShell {
-          buildInputs = [ stellar-cli komet rustWithWasmTarget ];
+          buildInputs = [ pkgs.stellar-cli komet rustWithWasmTarget ];
 
           shellHook = ''
             ${pkgs.lib.strings.optionalString
